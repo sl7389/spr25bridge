07e3392617f5925817b21d88965bbbb21b589579
sl7389 <95985914+sl7389@users.noreply.github.com>
2025-05-15 17:46:27 -0500

    Add files via upload

---
+++ hw4q1-for.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    int n;
+    int even;
+    int counter;
+    std::cout<<"please enter a positive integer: ";
+    std::cin>>n;
+   /* counter = 1;
+    while (counter<=n){
+        std::cout<<2*counter<<std::endl;
+        counter++;
+    }
+    */
+    for(counter=1;counter<=n;counter++)
+        std::cout<<2*counter<<std::endl;
+    
+    
+    return 0;
+    
+}
